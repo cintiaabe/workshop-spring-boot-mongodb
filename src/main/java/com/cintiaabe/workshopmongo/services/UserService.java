f4852b70d18aec7e49736d94af78a213f54ba51f
Cintia Abe <cintia.rossetto@gmail.com>
2022-11-24 14:51:33 -0300

    Obtendo um usuário por id

--- src/main/java/com/cintiaabe/workshopmongo/services/UserService.java
+++ src/main/java/com/cintiaabe/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.cintiaabe.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cintiaabe.workshopmongo.domain.User;
 import com.cintiaabe.workshopmongo.repository.UserRepository;
+import com.cintiaabe.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
-
+	
+	public User findById (String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
